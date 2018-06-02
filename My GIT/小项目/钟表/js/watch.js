682d98a134b6afad137cf5eb8b710552ce41cc6d
Administrator <1635872541@qq.com>
2018-06-02 19:15:05 +0800

    钟表完成

--- My GIT/小项目/钟表/js/watch.js
+++ My GIT/小项目/钟表/js/watch.js
@@ -4,6 +4,13 @@
 var i=0;
 var oCainter=document.getElementById('container');
 var ul=document.createElement('ul');
+var oHour=document.createElement('div');
+oHour.className='hour';
+var oMinute=document.createElement('div');
+oMinute.className='minute';
+var oSecond=document.createElement('div');
+oSecond.className='second';
+
 while (i<60){
     var li=document.createElement('li');
     ul.appendChild(li);
@@ -12,4 +19,28 @@ while (i<60){
     i++
 }
 oCainter.appendChild(ul);
+oCainter.appendChild(oHour);
+oCainter.appendChild(oMinute);
+oCainter.appendChild(oSecond);
 
+var day=new Date();
+var dayHour=day.getHours();
+var dayMinute=day.getMinutes();
+var daySecond=day.getSeconds();
+oHour.style.transform='rotate('+(dayHour%12)*30+'deg)';
+oMinute.style.transform='rotate('+dayMinute*6+'deg)';
+oSecond.style.transform='rotate('+daySecond*6+'deg)';
+console.log(dayHour,dayMinute,daySecond);
+setInterval(function () {
+        daySecond++;
+    oSecond.style.transform='rotate('+daySecond*6+'deg)';
+    if (daySecond%60==0){
+        dayMinute++;
+        oMinute.style.transform='rotate('+dayMinute*6+'deg)';
+    }
+    if (dayMinute%60==0){
+        dayHour++;
+        oHour.style.transform='rotate('+(dayHour%12)*30+'deg)';
+    }
+        console.log(dayHour)
+},1000);
