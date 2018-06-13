dab75d194399e44e8a385107e515d2a886f87bfd
Administrator <1635872541@qq.com>
2018-06-13 11:34:58 +0800

    扫雷二基本完成，尚未添加图片

--- My GIT/小项目/扫雷2/js/index.js
+++ My GIT/小项目/扫雷2/js/index.js
@@ -10,9 +10,13 @@ var num=[
     '6','8','10'
 ];
 var count=0;
+var type=0;
+var winWay=0;
+var clickNum=0;
 for (var i=0;i<aButton.length;i++){
     aButton[i].index=i;
-    aButton[i].onclick=function () {
+    aButton[i].addEventListener('click',function () {
+        clickNum=0;
         oTable.innerHTML='';
         oTable.className='content';
         $(oTable).addClass('container-'+model[this.index]);
@@ -21,6 +25,7 @@ for (var i=0;i<aButton.length;i++){
             var oTr=document.createElement('tr');
             for(var k=0;k<num[this.index];k++){
                 var oTd= document.createElement('td');
+                $(oTd).addClass('mark');
                 oTd.innerHTML='<span></span>';
                 $(oTd).attr('date-row',''+j);
                 $(oTd).attr('date-col',''+k);
@@ -30,8 +35,29 @@ for (var i=0;i<aButton.length;i++){
         }
         var aTd=document.getElementsByTagName('td');
         count=aTd.length;
+        type=num[this.index];
+        winWay=count-type;
         mine(count,num[this.index]);
-    };
+        for(var i=0;i<num[this.index];i++){
+            var ri=$('.mine:eq('+i+')').attr('date-row')*1;
+            var ci=$('.mine:eq('+i+')').attr('date-col')*1;
+            addNum(ri-1,ci-1,num[this.index]);
+            addNum(ri-1,ci*1,num[this.index]);
+            addNum(ri-1,ci*1+1,num[this.index]);
+            addNum(ri,ci-1,num[this.index]);
+            addNum(ri,ci*1+1,num[this.index]);
+            addNum(ri*1+1,ci-1,num[this.index]);
+            addNum(ri*1+1,ci*1,num[this.index]);
+            addNum(ri*1+1,ci*1+1,num[this.index]);
+        }
+        $('.result').css({
+            display:'none'
+        });
+        $('.result span').css({
+            display:'none'
+        });
+    });
+
 }
 
 function mine(idx,onum) {
@@ -46,3 +72,78 @@ function mine(idx,onum) {
         $('td').eq(arr[z]).addClass('mine')
     }
 }
+function addNum(ri,ci,k) {
+    var idx=ri*k+ci;
+    if(ri < 0 || ri >= k || ci < 0 || ci >= k){
+        return
+    }else{
+        if ($('td:eq('+idx+')').hasClass('mine')){
+            return;
+        }else {
+            var str=$('td:eq('+idx+')').children().text()*1+1;
+            $('td:eq('+idx+')').children().text(str);
+        }
+    }
+}
+function open(ri,ci,k) {
+    if (ri < 0 || ri >= k || ci < 0 || ci >= k) {
+        return;
+    }
+    var i = ri * k + ci;
+    $('td:eq(' + i + ')').trigger('click');
+}
+
+
+$(document).on('click','.mark',function () {
+    $(this).removeClass('mark');
+    clickNum++;
+    $(this).css('background-color','white');
+    $(this).children().css({
+        // background:'red',
+        opacity:'1'
+    });
+        if ($(this).hasClass('mine')){
+            $('.result').css({
+                display:'block'
+            });
+            $('.result-lost').css({
+                display:'block'
+            });
+            return
+        }else {
+            if($(this).children().text()==''){
+                var ri=$(this).attr('date-row')*1;
+                var ci=$(this).attr('date-col')*1;
+                open(ri-1,ci-1,type);
+                open(ri - 1, ci,type);
+                open(ri - 1, ci + 1,type);
+                open(ri, ci - 1,type);
+                open(ri, ci + 1,type);
+                open(ri + 1, ci - 1,type);
+                open(ri + 1, ci,type);
+                open(ri + 1, ci + 1,type);
+            }
+        }
+    console.log(clickNum);
+    if (clickNum>=winWay){
+        $('.result').css({
+            display:'block'
+        });
+        $('.result-win').css({
+            display:'block'
+        });
+    }
+}).on('contextmenu','.mark',function (e) {
+    if(e.which==3){
+        $(this).toggleClass('flag')
+    }
+    if($('.mine[class~="flag"]').length==type&&$('.flag').length==type){
+        $('.result').css({
+            display:'block'
+        });
+        $('.result-win').css({
+            display:'block'
+        });
+    }
+    return false
+});
